946ab602d45b4a7783c12ccd562286c6204eb25d
olegovich2 <19922403wawa@gmail.com>
2025-05-14 13:15:30 +0300

    autoCompressorv1.0

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
 const path = require("path");
+const os = require("os");
 const fs = require("fs");
 const zlib = require("zlib"); // Для сжатия
 const readlineSync = require("readline-sync"); // Для введения в консоли пользователем
@@ -6,39 +7,135 @@ var any = true;
 
 const funcAutoCompressor = () => {
   console.log("==== AutoCompressor ====");
+  let platform = process.platform;
   const choice = readlineSync.question(
-    `Do you want to (c)ompress or (d)ecompress a file?`
+    `Do you want to (c)ompress, (d)ecompress or (del)ete a file?`
   );
   if (choice.toLowerCase() === "c") {
-    questionPath();
+    questionPath(platform);
   } else if (choice.toLowerCase() === "d") {
-    decompressFile();
+    decompressPath(platform);
+  } else if (choice.toLowerCase() === "del") {
+    deletePath(platform);
   } else {
     console.log('Invalid choice. Please enter "c" or "d".');
     funcAutoCompressor();
   }
 };
 
-const questionPath = () => {
+const questionPath = (platform) => {
+  let reg = /\\/g;
   const inputPath = readlineSync.question(
     "Enter the path of the file to compress: "
   );
-  if (!fs.existsSync(inputPath)) {
-    console.log(
-      `That file doesn't exist. Please check the path and try again. \n \n \n`
-    );
-    return funcAutoCompressor();
+  if (platform === "win32") {
+    let path = inputPath.replace(/\\/g, "/");
+    if (!fs.existsSync(path)) {
+      console.log(
+        `That file doesn't exist. Please check the path and try again.   `
+      );
+      return funcAutoCompressor();
+    }
+    getFilesPr(path);
+  } else {
+    if (reg.test(inputPath)) {
+      let path = inputPath.replace(/\\/g, "/");
+      if (!fs.existsSync(path)) {
+        console.log(
+          `That file doesn't exist. Please check the path and try again.   `
+        );
+        return funcAutoCompressor();
+      }
+      getFilesPr(path);
+    } else {
+      if (!fs.existsSync(inputPath)) {
+        console.log(
+          `That file doesn't exist. Please check the path and try again.   `
+        );
+        return funcAutoCompressor();
+      }
+      getFilesPr(inputPath);
+    }
   }
-  getFilesPr(inputPath);
+};
+
+const deletePath = (platform) => {
+  let reg = /\\/g;
+  const inputPath = readlineSync.question(
+    "Enter the path of the file to delete: "
+  );
+  if (platform === "win32") {
+    let path = inputPath.replace(/\\/g, "/");
+    if (!fs.existsSync(path)) {
+      console.log(
+        `That file doesn't exist. Please check the path and try again.   `
+      );
+      return funcAutoCompressor();
+    }
+    deleteOrDecompressFilesPr(path, "del");
+  } else {
+    if (reg.test(inputPath)) {
+      let path = inputPath.replace(/\\/g, "/");
+      if (!fs.existsSync(path)) {
+        console.log(
+          `That file doesn't exist. Please check the path and try again.   `
+        );
+        return funcAutoCompressor();
+      }
+      deleteOrDecompressFilesPr(path, "del");
+    } else {
+      if (!fs.existsSync(inputPath)) {
+        console.log(
+          `That file doesn't exist. Please check the path and try again.   `
+        );
+        return funcAutoCompressor();
+      }
+      deleteOrDecompressFilesPr(inputPath, "del");
+    }
+  }
+};
+
+const decompressPath = (platform) => {
+  let reg = /\\/g;
+  const inputPath = readlineSync.question(
+    "Enter the path of the file to decompress: "
+  );
+  if (platform === "win32") {
+    let path = inputPath.replace(/\\/g, "/");
+    if (!fs.existsSync(path)) {
+      console.log(
+        `That file doesn't exist. Please check the path and try again.   `
+      );
+      return funcAutoCompressor();
+    }
+    deleteOrDecompressFilesPr(path, "d");
+  } else {
+    if (reg.test(inputPath)) {
+      let path = inputPath.replace(/\\/g, "/");
+      if (!fs.existsSync(path)) {
+        console.log(
+          `That file doesn't exist. Please check the path and try again.   `
+        );
+        return funcAutoCompressor();
+      }
+      deleteOrDecompressFilesPr(path, "d");
+    } else {
+      if (!fs.existsSync(inputPath)) {
+        console.log(
+          `That file doesn't exist. Please check the path and try again.   `
+        );
+        return funcAutoCompressor();
+      }
+      deleteOrDecompressFilesPr(inputPath, "d");
+    }
+  }
 };
 
 const createGZ = async (string) => {
   try {
     any = false;
     const object = JSON.parse(string);
-    console.log(
-      `******Scan File: ${object.filePath}_____${object.type}******\n`
-    );
+    console.log(`******Scan File: ${object.filePath}_____${object.type}******`);
 
     const outputPath = object.filePath + ".gz";
     console.log("Starting compression...");
@@ -55,10 +152,10 @@ const createGZ = async (string) => {
         const percentage = ((1 - compressedSize / originalSize) * 100).toFixed(
           2
         );
-        console.log("\nCompression complete!");
+        console.log(`Compression complete!`);
         console.log(`Original size: ${formatSize(originalSize)}`);
         console.log(`Compressed size: ${formatSize(compressedSize)}`);
-        console.log(`You saved: ${percentage}% of space\n`);
+        console.log(`You saved: ${percentage}% of space${os.EOL}`);
         any = true;
       })
       .on("error", (error) => {
@@ -69,21 +166,55 @@ const createGZ = async (string) => {
   }
 };
 
+const decompressFile = async (object, param) => {
+  try {
+    any = param;
+    console.log(`******Scan File: ${object.gzPath}_____${object.type}******`);
+
+    let outputPath = object.gzPath.endsWith(".gz")
+      ? object.gzPath.slice(0, -3)
+      : object.gzPath + ".decompressed";
+    console.log(`Starting decompression...`);
+
+    const readStream = fs.createReadStream(object.gzPath);
+    const writeStream = fs.createWriteStream(outputPath);
+    const gunzip = zlib.createGunzip();
+
+    readStream
+      .pipe(gunzip)
+      .pipe(writeStream)
+      .on("finish", () => {
+        console.log("\nDecompression complete!");
+        console.log(`Output file: ${outputPath}${os.EOL}`);
+        fs.unlink(`${object.gzPath}`, (err) => {
+          if (err) throw err;
+          console.log(`Deleted______${object.gzPath}${os.EOL}`);
+        });
+        any = true;
+      })
+      .on("error", (error) => {
+        console.log("Something went wrong:", error.message);
+        console.log("This might not be a valid compressed file.");
+      });
+  } catch (error) {
+    console.log(error);
+  }
+};
+
 const getFilesPr = async (dir) => {
   try {
     if (!dir.includes(".git")) {
       if (fs.statSync(dir).isDirectory()) {
-        console.log(`\n******Scan Folder: ${dir}******\n`);
+        console.log(`******Scan Folder: ${dir}******`);
         const files = await fs.promises.readdir(dir).then((result) => {
           result.forEach((item) => {
-            if (fs.statSync(`${dir}/${item}`).isDirectory()) {
-              getFilesPr(`${dir}/${item}`);
+            let path = `${dir}/${item}`;
+            if (fs.statSync(`${path}`).isDirectory()) {
+              getFilesPr(`${path}`);
             } else {
               new Promise((resolve, reject) => {
-                setImmediate(() => {
-                  resolve(`${dir}/${item}`);
-                });
-              }).then(async (result) => {
+                resolve(`${dir}/${item}`);
+              }).then((result) => {
                 timeAndPath(result);
               });
             }
@@ -91,12 +222,62 @@ const getFilesPr = async (dir) => {
         });
       } else {
         new Promise((resolve, reject) => {
-          setImmediate(() => {
-            resolve(`${dir}`);
-          });
-        }).then(async (result) => {
+          resolve(`${dir}`);
+        }).then((result) => {
           timeAndPath(result);
         });
+      }
+    } else {
+      throw new Error("Path contains .git. Try changing path");
+    }
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+const deleteOrDecompressFilesPr = async (dir, str) => {
+  try {
+    if (!dir.includes(".git")) {
+      if (fs.statSync(dir).isDirectory()) {
+        console.log(`******Scan Folder: ${dir}******`);
+        const files = await fs.promises.readdir(dir).then((result) => {
+          result.forEach((item) => {
+            let path = `${dir}/${item}`;
+            if (fs.statSync(`${path}`).isDirectory()) {
+              deleteOrDecompressFilesPr(`${path}`);
+            } else {
+              if (path.includes(".gz") && str === "del") {
+                fs.unlink(`${path}`, (err) => {
+                  if (err) throw err;
+                  console.log(`Deleted______${path}`);
+                });
+              } else if (path.includes(".gz") && str === "d") {
+                new Promise((resolve, reject) => {
+                  resolve(`${path}`);
+                }).then((result) => {
+                  timeAndPath(result, {}, str);
+                });
+              } else if (!path.includes(".gz") && str === "del") {
+                console.log("Пока нечего удалять");
+              } else if (!path.includes(".gz") && str === "d") {
+                console.log("Пока нечего разжимать");
+              }
+            }
+          });
+        });
+      } else {
+        if (dir.includes(".gz") && str === "del") {
+          fs.unlink(`${dir}`, (err) => {
+            if (err) throw err;
+            console.log(`Deleted______${dir}`);
+          });
+        } else if (dir.includes(".gz") && str === "d") {
+          new Promise((resolve, reject) => {
+            resolve(`${dir}`);
+          }).then((result) => {
+            timeAndPath(result, {}, str);
+          });
+        }
       }
     } else {
       throw new Error("Path contains .git. Try changing path");
@@ -116,7 +297,7 @@ const formatSize = (bytes) => {
   }
 };
 
-const timeAndPath = (path, object) => {
+const timeAndPath = (path, object, param) => {
   try {
     var object = object || {};
     if (!path.includes(".gz") && !object.type) {
@@ -137,11 +318,26 @@ const timeAndPath = (path, object) => {
         }).then((result) => {
           createGZ(`${result}`);
         });
-      } else if (object.error && object.type === "archive") return console.log(`______NO FILE______`);
+      } else if (object.error && object.type === "archive" && param === "d") {
+        if (any === true) {
+          any = false;
+          return resultObjectForDecompress(object, "nofile");
+        } else {
+          return sleepFour(object, "nofile", 500);
+        }
+      }
+
       if (!path.includes(".gz")) object.fileTime = stat.mtime;
       else object.gzTime = stat.mtime;
-
-      if (object.gzTime && object.fileTime) resultObject(object);
+      if (object.gzTime && object.fileTime && param === "d") {
+        if (any === true) {
+          any = false;
+          return resultObjectForDecompress(object, "");
+        } else {
+          return sleepFour(object, "", 500);
+        }
+      } else if (object.gzTime && object.fileTime && !param)
+        resultObject(object);
       else if (!object.gzTime) {
         new Promise((resolve, reject) => {
           setImmediate(() => {
@@ -150,13 +346,21 @@ const timeAndPath = (path, object) => {
         }).then((result) => {
           timeAndPath(result, object);
         });
-      } else if (!object.fileTime) {
+      } else if (!object.fileTime && !param) {
         new Promise((resolve, reject) => {
           setImmediate(() => {
             resolve(`${object.gzPath.replace(".gz", "")}`);
           });
         }).then((result) => {
           timeAndPath(result, object);
+        });
+      } else if (!object.fileTime && param === "d") {
+        new Promise((resolve, reject) => {
+          setImmediate(() => {
+            resolve(`${object.gzPath.replace(".gz", "")}`);
+          });
+        }).then((result) => {
+          timeAndPath(result, object, "d");
         });
       }
     });
@@ -173,7 +377,7 @@ const resultObject = async (object) => {
           `******Scan File: ${object.filePath}_____${object.type}******`
         );
         console.log(
-          `______FILE:${object.filePath} HAS AN ARCHIVE, BUT THE ARCHIVE IS OUT OF THE DATE______\n`
+          `______FILE:${object.filePath} HAS AN ARCHIVE, BUT THE ARCHIVE IS OUT OF THE DATE______`
         );
         return new Promise(async (resolve, reject) => {
           if (any === true) resolve(`${JSON.stringify(object)}`);
@@ -193,7 +397,7 @@ const resultObject = async (object) => {
           `******Scan File: ${object.gzPath}_____${object.type}******`
         );
         console.log(
-          `______FILE:${object.gzPath} ARCHIVE IS OUT OF THE DATE______\n`
+          `______FILE:${object.gzPath} ARCHIVE IS OUT OF THE DATE______`
         );
         return new Promise(async (resolve, reject) => {
           object.filePath = object.filePath.replace(".gz", "");
@@ -211,6 +415,50 @@ const resultObject = async (object) => {
     }
   } catch (error) {
     console.log(error);
+  }
+};
+
+const resultObjectForDecompress = async (object, param) => {
+  try {
+    if (param === "nofile") {
+      console.log(
+        `*************No relevant file found, starting archive decompression***************`
+      );
+      decompressFile(object, "false");
+    } else {
+      if (object.fileTime > object.gzTime) {
+        console.log(
+          `______FILE:${object.fileTime} ARCHIVE IS OUT OF THE DATE______`
+        );
+        const result = actualFile();
+        if (result === "y") {
+          decompressFile(object, "false");
+        } else {
+          any = true;
+        }
+      } else {
+        console.log(
+          `______FILE:${object.fileTime} ARCHIVE IS UP-TO-DATE______`
+        );
+        decompressFile(object, "false");
+      }
+    }
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+const actualFile = () => {
+  const choice = readlineSync.question(
+    `Do you want to decompress archive, because the file is more current than the archive? Press (y)es or (n)o:`
+  );
+  if (choice === "y") {
+    return "y";
+  } else if (choice === "n") {
+    return "n";
+  } else {
+    console.log("You entered an invalid character. Try again.");
+    actualFile();
   }
 };
 
@@ -244,7 +492,7 @@ function sleepTwo(object, ms) {
 const awaitFunctionTwo = (object) => {
   if (any === true) {
     console.log(`******Scan File: ${object.gzPath}_____${object.type}******`);
-    console.log(`______FILE:${object.gzPath} ARCHIVE IS UP-TO-DATE______\n`);
+    console.log(`______FILE:${object.gzPath} ARCHIVE IS UP-TO-DATE______`);
   } else {
     sleepTwo(object, 500);
   }
@@ -262,10 +510,27 @@ const awaitFunctionThree = (object) => {
   if (any === true) {
     console.log(`******Scan File: ${object.filePath}_____${object.type}******`);
     console.log(
-      `______FILE:${object.filePath} HAS AN ARCHIVE, BUT THE ARCHIVE IS UP-TO-DATE______\n`
+      `______FILE:${object.filePath} HAS AN ARCHIVE, BUT THE ARCHIVE IS UP-TO-DATE______`
     );
   } else {
     sleepThree(object, 500);
+  }
+};
+
+function sleepFour(object, param, ms) {
+  return new Promise((resolve) =>
+    setTimeout(() => {
+      awaitFunctionFour(object, param);
+    }, ms)
+  );
+}
+
+const awaitFunctionFour = (object, param) => {
+  if (any === true) {
+    any = false;
+    resultObjectForDecompress(object, param);
+  } else {
+    sleepFour(object, "nofile", 500);
   }
 };
 
