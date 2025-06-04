783a31e1a88f0f5a24d6168d93aacc6fbb6c0835
olegovich2 <19922403wawa@gmail.com>
2025-06-04 16:16:05 +0300

    sqlExplorerv1.0

---
+++ public/js/getDataFromForm.js
@@ -0,0 +1,31 @@
+import { getDatabases, postDataForRequestToDB } from "./requests.js";
+
+const selectForListDB = document.querySelector('[data-select="listDB"]');
+const formForRequest = document.querySelector(
+  '[data-form="formForSendOnServer"]'
+);
+const textareaForRequest = document.querySelector('[name="textareaRequest"]');
+
+export const dataToSelect = (object) => {
+  selectForListDB.innerHTML = "";
+  object.arrayDB.forEach((element) => {
+    let newOption = new Option(`${element}`, `${element}`);
+    selectForListDB.add(newOption);
+  });
+};
+
+export const getDataFromForm = (event) => {
+  event.preventDefault();
+  const object = {};
+  if (!!event.target.dataset.button) {
+    object.req = textareaForRequest.value.toUpperCase();
+    object.DB = selectForListDB.value;
+    postDataForRequestToDB("/reqToDB", JSON.stringify(object));
+    formForRequest.reset();
+  }
+};
+
+formForRequest.addEventListener("click", getDataFromForm);
+document.addEventListener("DOMContentLoaded", () => {
+  getDatabases("/getDB");
+});
